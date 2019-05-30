b43032dca73d43195ac9eea2743a732a2e37e230
Nemanja <themenkoprojects@gmail.com>
2019-05-30 01:14:35 +0000

    sockets, user messages and more

--- toplist.cpp
+++ toplist.cpp
@@ -1,5 +1,3 @@
-
-
 #include "toplist.h"
 
 #define	FIND_IFACE(func, assn_var, num_var, name, type) \
@@ -20,6 +18,7 @@
 #define LOADINTERFACE(_module_, _version_, _out_) Sys_LoadInterface(_module_, _version_, NULL, reinterpret_cast<void**>(& _out_ ))
 
 Toplist g_Toplist;
+
 ToplistListener g_Listener;
 
 PLUGIN_EXPOSE(Toplist, g_Toplist);
@@ -31,6 +30,25 @@ SH_DECL_HOOK2_void(IServerGameClients, ClientCommand, SH_NOATTRIB, 0, edict_t*, 
 SH_DECL_HOOK0_void(IServerGameDLL, LevelShutdown, SH_NOATTRIB, 0);
 SH_DECL_HOOK6(IServerGameDLL, LevelInit, SH_NOATTRIB, 0, bool, char const *, char const *, char const *, char const *, bool, bool);
 
+Toplist* Toplist::pToplist = nullptr;
+
+static userMessageInfo g_userMessages[255];
+static int g_iMaxUserMessages = 0;
+
+static int UserMessageFromName(const char *pName)
+{
+	if (!pName)
+		return -1;
+
+	for (int i = 0; i < g_iMaxUserMessages; i++)
+	{
+		if (strcmp(pName, g_userMessages[i].msgName) == 0)
+			return g_userMessages[i].messageID;
+	}
+
+	return -1;
+}
+
 bool Toplist::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool late) {
 
 	m_CurrentPlayers = 0;
@@ -40,8 +58,17 @@ bool Toplist::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool 
 	char iface_buffer[255];
 	int num = 0;
 
-	//load server ip
+	// logger
+	plog::init(plog::debug, "kz/addons/metamod/toplist/log.txt");
+
 	m_ToplistIP = getToplistIP();
+
+	// server & server ip
+	pToplist = this;
+
+	std::thread srv(&Toplist::startServer, this, m_ToplistIP);
+	srv.detach();
+
 
 	// load interface
 	loadCustomCvar = !!(LOADINTERFACE("vstdlib.dll", CVAR_INTERFACE_VERSION, cvar)); // get the interface for customCvar.
@@ -73,10 +100,25 @@ bool Toplist::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, bool 
 	
 	ismm->AddListener(this, &g_Listener);
 
+	while (true)
+	{
+		int dummy;
+		if (!m_ServerDll->GetUserMessageInfo(g_iMaxUserMessages, g_userMessages[g_iMaxUserMessages].msgName, sizeof(g_userMessages[g_iMaxUserMessages].msgName), dummy))
+			break;
+
+			g_userMessages[g_iMaxUserMessages].messageID = g_iMaxUserMessages;
+
+			g_iMaxUserMessages++;
+	}
+
+
 	return true;
 
 }
 bool Toplist::Unload(char *error, size_t maxlen) {
+
+	filter.RemoveAllRecipients();
+	m_Service.stop();
 	m_CurrentPlayers = 0;
 	m_GameEventManager->RemoveListener(this);
 	return true;
@@ -94,7 +136,7 @@ void Toplist::AllPluginsLoaded() {
 	m_GameEventManager->AddListener(this, "player_starttimer2", true);
 	m_GameEventManager->AddListener(this, "player_stoptimer", true);
 	m_GameEventManager->AddListener(this, "player_stoptimer2", true);
-
+	 
 
 	
 }
@@ -109,7 +151,7 @@ bool Toplist::OnLevelInit(char const *pMapName,char const *pMapEntities,char con
 
 bool Toplist::registerMap() {
 	try {
-		http::Request request(m_ToplistIP + "mapregistration.php");
+		http::Request request("http://" + m_ToplistIP + "/toplist/mapregistration.php");
 		http::Response response;
 
 		// send a post request
@@ -130,7 +172,7 @@ bool Toplist::registerMap() {
 
 bool Toplist::registerPlayer(std::string steamid, std::string player) {
 	try {
-		http::Request request(m_ToplistIP + "registerplayer.php");
+		http::Request request("http://" + m_ToplistIP + "/toplist/registerplayer.php");
 		http::Response response;
 
 		// send a post request
@@ -152,7 +194,7 @@ bool Toplist::registerPlayer(std::string steamid, std::string player) {
 bool Toplist::addRecord(std::string mapName, const char *courseName, std::string steamID, std::string playerName, int seconds, int miliseconds, int checkpoints, int teleports) {
 
 	try {
-		http::Request request(m_ToplistIP + "addrecord.php");
+		http::Request request("http://" + m_ToplistIP + "/toplist/addrecord.php");
 		http::Response response;
 
 		// send a post request
@@ -179,7 +221,6 @@ bool Toplist::addRecord(std::string mapName, const char *courseName, std::string
 
 void Toplist::ClientPutInServer(edict_t *pEntity, char const *playername) {
 
-
 	m_Players.push_back(s_Players());
 	
 	m_Players[m_CurrentPlayers].Client = pEntity;
@@ -188,8 +229,10 @@ void Toplist::ClientPutInServer(edict_t *pEntity, char const *playername) {
 	
 	std::thread pRegister(&Toplist::registerPlayer, this, m_Engine->GetPlayerNetworkIDString(m_Players[m_CurrentPlayers].Client), m_Players[m_CurrentPlayers].playername);
 	pRegister.detach();
-	
+
 	++m_CurrentPlayers;
+
+	filter.AddRecipient(m_CurrentPlayers);
 
 }
 
@@ -224,7 +267,7 @@ void Toplist::ClientDisconnect(edict_t *pEntity) {
 		}
 	}
 
-	
+	filter.RemoveRecipient(m_CurrentPlayers);
 
 	RETURN_META(MRES_IGNORED);
 }
@@ -291,7 +334,7 @@ std::string Toplist::getToplistIP() {
 	std::ifstream info;
 	std::string line;
 	std::smatch match;
-	std::regex rgx("TOPLIST_IP (http://[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/toplist/)");
+	std::regex rgx("TOPLIST_IP ([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})");
 	std::string ip;
 
 	info.open("kz/addons/metamod/toplist/info.txt");
@@ -301,19 +344,328 @@ std::string Toplist::getToplistIP() {
 		{
 			if (std::regex_search(line, match, rgx))
 			{
-				ConMsg("\nRegex: %s\n", match.str(1));
 				ip = match.str(1);
 			}
 			else {
-				ConMsg("\n[TOPLIST ERROR] Invalid adress form in info file.\n");
-				ip = "Invalid.";
+				ip = "127.0.0.1";
 			}
 		}
 	}
 	else {
-		ConMsg("\n[TOPLIST ERROR] Failed to open file\n");
+		ConMsg("\n[TOPLIST ERROR] Failed to open info file\n");
 	}
 	info.close();
 
 	return ip;
+}
+
+void Toplist::sendDataToGame(std::string data) {
+
+	char msgText[2048];
+	*msgText = 0;
+
+	int n = data.length();
+	char *recMsg = new char[n + 1];
+	strcpy(recMsg, data.c_str());
+
+	strcat_s(msgText, recMsg);
+
+	ColorClientPrint(filter, HUD_PRINTTALK, 1, msgText);
+
+	delete recMsg;
+}
+
+void Toplist::startServer(std::string ip) {
+
+	boost::asio::io_service::work serviceWork(m_Service);
+
+	PLOGD << "[TOPLIST] - Running socket server...\n";
+	m_Server = std::make_shared<server>(m_Service, "127.0.0.1", 5000);
+
+	m_Service.run();
+}
+
+
+void Toplist::UserMessageBegin(IRecipientFilter &filter, const char *messagename) {
+
+	if (g_pMsgBuffer)
+	{
+		Warning("Tried to start a new usermessage while one hasn't been sent. This has caused a memory leak.");
+	}
+
+	g_pMsgBuffer = NULL;
+
+	int id = UserMessageFromName(messagename);
+
+	if (id == -1)
+	{
+		Warning("Specified unknown usermessage name %s, can't send usermessage.\n", messagename);
+	}
+
+	g_pMsgBuffer = m_Engine->UserMessageBegin(&filter, id);
+}
+
+void Toplist::MessageEnd(void)
+{
+	m_Engine->MessageEnd();
+
+	g_pMsgBuffer = NULL;
+}
+
+void Toplist::MessageWriteByte(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteByte(iValue);
+}
+
+void Toplist::MessageWriteChar(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteChar(iValue);
+}
+
+void Toplist::MessageWriteShort(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteShort(iValue);
+}
+
+void Toplist::MessageWriteWord(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteWord(iValue);
+}
+
+void Toplist::MessageWriteLong(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteLong(iValue);
+}
+
+void Toplist::MessageWriteFloat(float flValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteFloat(flValue);
+}
+
+void Toplist::MessageWriteAngle(float flValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBitAngle(flValue, 8);
+}
+
+void Toplist::MessageWriteCoord(float flValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBitCoord(flValue);
+}
+
+void Toplist::MessageWriteVec3Coord(const Vector& rgflValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBitVec3Coord(rgflValue);
+}
+
+void Toplist::MessageWriteVec3Normal(const Vector& rgflValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBitVec3Normal(rgflValue);
+}
+
+void Toplist::MessageWriteAngles(const QAngle& rgflValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBitAngles(rgflValue);
+}
+
+void Toplist::MessageWriteString(const char *sz)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteString(sz);
+}
+
+void Toplist::MessageWriteEntity(int iValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteShort(iValue);
+}
+
+void Toplist::MessageWriteEHandle(CBaseEntity *pEntity)
+{
+	Warning("TODO MessageWriteEHandle - not implemented.\n");
+}
+
+// bitwise
+void Toplist::MessageWriteBool(bool bValue)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteOneBit(bValue ? 1 : 0);
+}
+
+void Toplist::MessageWriteUBitLong(unsigned int data, int numbits)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteUBitLong(data, numbits);
+}
+
+void Toplist::MessageWriteSBitLong(int data, int numbits)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteSBitLong(data, numbits);
+}
+
+void Toplist::MessageWriteBits(const void *pIn, int nBits)
+{
+	if (!g_pMsgBuffer)
+	{
+		Warning("WRITE_ usermessage called with no active message\n");
+		return;
+	}
+
+	g_pMsgBuffer->WriteBits(pIn, nBits);
+}
+
+// Sends a text message to a player (or players) specified by the recipient filter. To use:
+// CRecipientFilter filter;
+// filter.AddRecipient( player entity index ) as many as you want.
+// Then do ClientPrint( filter, etc... )
+//
+// Example: ClientPrint( filter, HUD_PRINTTALK, "Hello, this is a chat message" );
+void Toplist::ClientPrint(IRecipientFilter& filter, int msg_dest, const char *msg_name, const char *param1, const char *param2, const char *param3, const char *param4) {
+	UserMessageBegin(filter, "TextMsg");
+	WRITE_BYTE(msg_dest);
+	WRITE_STRING(msg_name);
+
+	if (param1)
+		WRITE_STRING(param1);
+	else
+		WRITE_STRING("");
+
+	if (param2)
+		WRITE_STRING(param2);
+	else
+		WRITE_STRING("");
+
+	if (param3)
+		WRITE_STRING(param3);
+	else
+		WRITE_STRING("");
+
+	if (param4)
+		WRITE_STRING(param4);
+	else
+		WRITE_STRING("");
+
+	MessageEnd();
+}
+
+void Toplist::ColorClientPrint(IRecipientFilter& filter, int msg_dest, int bChat, const char *msg_name, const char *param1, const char *param2, const char *param3, const char *param4) {
+	
+	UserMessageBegin(filter, "SayText2");
+
+	WRITE_BYTE(0); // Entid of who's talking, 0 for world
+	WRITE_BYTE(bChat); // Act like a chat message and play sound if true, otherwise silent message false
+	WRITE_STRING(msg_name); // The message itself
+
+	if (param1)
+		WRITE_STRING(param1);
+	else
+		WRITE_STRING("");
+
+	if (param2)
+		WRITE_STRING(param2);
+	else
+		WRITE_STRING("");
+
+	if (param3)
+		WRITE_STRING(param3);
+	else
+		WRITE_STRING("");
+
+	if (param4)
+		WRITE_STRING(param4);
+	else
+		WRITE_STRING("");
+
+	MessageEnd();
 }
