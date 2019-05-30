7e84ff04bc9481d4f6b17e6e14201d70196c26a0
Nemanja <themenkoprojects@gmail.com>
2019-05-30 01:13:30 +0000

    sockets, user messages and more

--- toplist.h
+++ toplist.h
@@ -1,22 +1,38 @@
 #ifndef TOPLIST_H_
 #define TOPLIST_H_
 
-#define TOPLIST_VERSION "0.9.0"
+#define TOPLIST_VERSION "1.0.0"
 
 #include <iostream>
+#include <memory>
 #include <vector>
 #include <chrono>
 #include <thread>
 #include <fstream>
 #include <regex>
 
+// source engine
 #include <ISmmPlugin.h>
 #include <sourcehook/sourcehook.h>
 #include <igameevents.h>
 #include <convar.h>
 #include <iplayerinfo.h>
+#include <bitbuf.h>
 
+// boost
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#include "Include/pluginusermessages.hpp"
 #include "Include/HTTPRequest.hpp"
+#include "Include/server.hpp"
+#include "Include/plog/Log.h"
+#include "Include/Colors.hpp"
+
+#define HUD_PRINTNOTIFY		1
+#define HUD_PRINTCONSOLE	2
+#define HUD_PRINTTALK		3
+#define HUD_PRINTCENTER		4
 
 typedef struct Players {
 	edict_t *Client;
@@ -24,10 +40,19 @@ typedef struct Players {
 	const char *playername;
 } s_Players;
 
+struct userMessageInfo
+{
+	char msgName[255];
+	int messageID;
+};
+
+static bf_write *g_pMsgBuffer = nullptr;
 
 class Toplist : public ISmmPlugin, public IMetamodListener, public IGameEventListener2 {
 
 public:
+
+	Toplist() = default;
 
 	const char *GetAuthor() { return "Menko"; }
 	const char *GetName() { return "Toplist"; }
@@ -49,10 +74,41 @@ public:
 	void LevelShutdown(void);
 	void FireGameEvent(IGameEvent* event);
 
+	// test
+	CRecipientFilter filter;
+
 	// helpers
 	char *concat(const char *s1, const char *s2);
 	const char *getCourse(IGameEvent *event);
 	std::string getToplistIP();
+	void sendDataToGame(std::string data);
+	void UserMessageBegin(IRecipientFilter &filter, const char *messagename);
+	void ClientPrint(IRecipientFilter& filter, int msg_dest, const char *msg_name, const char *param1 = nullptr, const char *param2 = nullptr, const char *param3 = nullptr, const char *param4 = nullptr);
+	void ColorClientPrint(IRecipientFilter& filter, int msg_dest, int bChat, const char *msg_name, const char *param1 = nullptr, const char *param2 = nullptr, const char *param3 = nullptr, const char *param4 = nullptr);
+
+	///// chat message functions
+	// bitwise
+	void MessageWriteBits(const void *pIn, int nBits);
+	void MessageWriteSBitLong(int data, int numbits);
+	void MessageWriteUBitLong(unsigned int data, int numbits);
+	void MessageWriteBool(bool bValue);
+	// rest
+	void MessageWriteEHandle(CBaseEntity *pEntity);
+	void MessageWriteEntity(int iValue);
+	void MessageWriteString(const char *sz);
+	void MessageWriteAngles(const QAngle& rgflValue);
+	void MessageWriteVec3Normal(const Vector& rgflValue);
+	void MessageWriteVec3Coord(const Vector& rgflValue);
+	void MessageWriteCoord(float flValue);
+	void MessageWriteAngle(float flValue);
+	void MessageWriteFloat(float flValue);
+	void MessageWriteLong(int iValue);
+	void MessageWriteWord(int iValue);
+	void MessageWriteShort(int iValue);
+	void MessageWriteChar(int iValue);
+	void MessageWriteByte(int iValue);
+	void MessageEnd(void);
+	/////
 
 	// metamod listener
 	bool OnLevelInit(char const *pMapName, char const *pMapEntities, char const *pOldLevel, char const *pLandmarkName, bool loadGame, bool background);
@@ -62,30 +118,40 @@ public:
 	bool registerPlayer(std::string steamid, std::string player);
 	bool addRecord(std::string mapName, const char *courseName, std::string steamID,std::string playerName, int seconds, int miliseconds, int checkpoints, int teleports);
 
-
+	 
 	void Hook_ClientCommand(edict_t *pEntity, const CCommand &args);
 
 public:
 	int GetApiVersion() { return METAMOD_PLAPI_VERSION; }
 
+	static Toplist *pToplist;
+
 private:
 
+	// source engine
 	IGameEventManager2 * m_GameEventManager;
 	IVEngineServer *m_Engine;
 	IServerGameDLL *m_ServerDll;
 	IServerGameClients *m_ServerClients;
-
 	IPlayerInfoManager *m_PlayerInfoManager;
-
 	IConVar *p_ConVar;
 
+	// my variables
 	std::string m_ToplistIP;
 	std::string currentMap;
 	int m_CurrentPlayers = 0;
 	std::vector<s_Players> m_Players;
 	bool loadCustomCvar;
 
+	// server
+	boost::asio::io_service m_Service;
+	std::shared_ptr<server> m_Server;
+
+
+	void startServer(std::string ip);
 };
+
+
 
 class ToplistListener : public IMetamodListener
 {
